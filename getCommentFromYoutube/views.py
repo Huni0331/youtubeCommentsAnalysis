a81434ef68a9d7e627737efd850ae8b83b43ff06
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2022-05-30 22:07:24 +0900

    send reviews to predict_views.py

--- getCommentFromYoutube/views.py
+++ getCommentFromYoutube/views.py
@@ -4,24 +4,12 @@ from getCommentFromYoutube.utillGetComment import getComments
 from django.views.decorators.csrf import csrf_exempt
 from django import template
 
-import numpy as np
-import pandas as pd
-from openpyxl import Workbook
-
-
-
-# 엑셀에 테이블 시트 생성
-wb = Workbook(write_only=True)
-ws = wb.create_sheet()
-
 strbun = template.Library()
 
 def inputSearchingUrl(request):
     # 메인 페이지(검색 페이지)
     return render(request, 'index.html', )
 
-
-global id
 
 @csrf_exempt
 @strbun.filter("geturl")
@@ -37,25 +25,8 @@ def getCommentsFromYoutube(request):
 
     reviews = getComments(geturl_form.split('v=')[1])
 
-    adf = pd.DataFrame(reviews, columns=['Comments'])
-
-    pd_data = {"댓글 내용": adf['Comments']}
-
-    youtube_pd = pd.DataFrame(pd_data)
-
-    id = geturl_form.split('v=')[1]
-
-    # 엑셀 파일 저장
-    youtube_pd.to_excel('getCommentFromYoutube/data/%s.xlsx' %(id))
-
-    exec(open('getCommentFromYoutube/predict_views.py', encoding="UTF-8").read())
-
-
-    # for review in reviews:
-    #     print(review)
-    #     print()
-
     return render(request, 'index2.html', context)
 
+# predict_views.py 로 reviews 를 보낼 때, 사용하는 함수
 def send_reviews() :
     return reviews
